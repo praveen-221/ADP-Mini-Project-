784d9b744a1700f04315455d632f74363969c41b
Gautham <72738249+Gautham-22@users.noreply.github.com>
2021-07-12 12:56:25 +0530

    deleted assets directory

--- assets/js/passCheck.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("pass-check").addEventListener("click",function() {
-    document.getElementById("pass-check").children[0].classList.toggle("hide");
-    document.getElementById("pass-check").children[1].classList.toggle("hide");
-    if(document.getElementById("pass").type=="password"){
-        document.getElementById("pass").type="text";
-    }else{
-        document.getElementById("pass").type="password";
-    }
-})
